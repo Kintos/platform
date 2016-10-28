d546a08a170204176dcb668f2de8428c0556d8a0
Diego <zdr_00@live.com>
2016-10-28 04:06:28 +0000

    basic sign up added

--- routes.py
+++ routes.py
@@ -1,11 +1,87 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+from forms import SignupForm, LoginForm
+
+import os
+import pyrebase
+#import openpay
+
+config = {
+  "apiKey": "AIzaSyBgbGvUXaV2Npjvr5A04AW48TwSpJvouuI",
+  "authDomain": "project-7682141674235767650.firebaseapp.com",
+  "databaseURL": "https://kinto.firebaseio.com",
+  "storageBucket": "project-7682141674235767650.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
 
 app = Flask(__name__)
 
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template("login.html", form = form)
+        else:
+            email = form.email.data
+            password = form.password.data
+            
+            user = auth.sign_in_with_email_and_password(email, password)
+            print(user)
+            
+    elif request.method == "GET":
+        return render_template("login.html", form = form)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template('signup.html', form = form)
+        else :
+            auth.create_user_with_email_and_password(form.email.data, form.password.data)
+            session["email"] = form.email.data
+            return redirect(url_for("home"))
+    
+    elif request.method == "GET":
+        return render_template("signup.html", form = form)
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+    
+@app.route("/mov")
+def mov():
+    return render_template("mov.html")
+
+    
+@app.route("/invest")
+def invest():
+    return render_template("invest.html")
+
+    
+@app.route("/loan")
+def loan():
+    return render_template("loan.html")
+
+
+@app.route("/support")
+def support():
+    return render_template("support.html")
+    
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
---
+++ static/js/actions_prestamo.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+          $("#slider").slider({
+              animate: true,
+              value:1000,
+              min: 1000,
+              max: 5000,
+              step: 250,
+              slide: function(event, ui) {
+                  update(1,ui.value);
+              }
+          });
+          $("#slider2").slider({
+              animate: true,
+              value: 1,
+              min: 1,
+              max: 4,
+              step: 1,
+              slide: function(event, ui) {
+                  update(2,ui.value);
+              }
+          });
+          $("#cantidad").val(1000);
+          $("#duracion").val(1);
+          $("#cantidad-label").text(1000);
+          $("#duracion-label").text(1);
+
+          update();
+      });
+      function update(slider,val) {
+        var $cantidad = slider == 1?val:$("#cantidad").val();
+        var $duracion = slider == 2?val:$("#duracion").val();
+        var $subtotal = ($cantidad / $duracion) * 1.3;
+         $total = "$" + $subtotal.toFixed(2);
+         $( "#cantidad" ).val($cantidad);
+         $( "#cantidad-label" ).text($cantidad);
+         $( "#duracion" ).val($duracion);
+         $( "#duracion-label" ).text($duracion);
+         $( "#total" ).val($total);
+         $( "#total-label" ).text($total);
+
+         $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$cantidad+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+         $('#slider2 a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$duracion+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+      }
